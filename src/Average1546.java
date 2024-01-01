cc3e5e9b14447d3efb3547910e163db55e8b96c1
woodawon <woodawon0602@gmail.com>
2024-01-01 21:02:31 +0900

    get start

---
+++ src/Average1546.java
@@ -0,0 +1,27 @@
+
+
+import java.util.Scanner;
+
+public class Average1546 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int A[] = new int[N];
+		
+		for(int i = 0;i < N;i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		long sum = 0;
+		long max = 0;
+		for (int i = 0; i < N;i++) {
+			if (A[i] > max) max = A[i];
+			sum += A[i];
+		}
+		System.out.println(sum * 100 / max / N); // 식 : 총합 * 100 / 최고점수 / 과목 개수 = 평균값 
+
+	}
+
+}
