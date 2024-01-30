d5669be2f22f9ab41f2833b6a2536e847a3782fd
woodawon <woodawon0602@gmail.com>
2024-01-30 23:55:44 +0900

    01/30

--- src/Recursive_Study.java
+++ src/Recursive_Study.java
@@ -1,18 +1,25 @@
+import java.util.ArrayList;
+
 /*
  * 재귀함수 공부 - 피보나치 수열, 팩토리얼
  * */
 public class Recursive_Study {
+	static ArrayList<Integer> list = new ArrayList<>();
 
 	public static void main(String[] args) {
 		
 		System.out.println("fibonacci : " + fibonacci(4));
 		System.out.println("factorial : " + factorial(3));
+		for(int i = 0; i < list.size(); i++) {
+			System.out.println("list : ");
+		}
 
 	}
 	
 	public static int fibonacci(int num) {
 		// 피보나치 수열 메서드
-		// 0 + 1 + 2 + 3 + 4 + 5... -> 0+1 = 1, 1+2=3, 3+3=6, 6+4=10
+		
+		Recursive_Study.list.add(num);
 		if(num == 0) {
 			return 0;
 		}
