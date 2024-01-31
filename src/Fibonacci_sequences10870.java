5ba1d4f54979e11822240591c0654d7be916b5e0
우다원 <105121244+woodawon@users.noreply.github.com>
2024-01-31 12:17:52 +0900

    Create Fibonacci_sequences10870.java
    01/31

---
+++ src/Fibonacci_sequences10870.java
@@ -0,0 +1,21 @@
+// Fibonacci sequences
+// 0 1 1 2 3 5 8 13 21 34 55 89 144 ...
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(method(sc.nextInt()));
+        sc.close();
+    }
+    public static int method(int num){
+        if(num == 0) {
+            return 0;
+        }
+        if(num == 1) {
+            return 1;
+        }
+        return method(num-1) + method(num-2);
+    }
+}
